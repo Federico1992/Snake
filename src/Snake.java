50dbc43a6e07c2104cdbd599e743cf1b9c2649e9
erasmusuk <erasmus_uk@outlook.com>
2014-04-23 15:58:23 +0100

    Update Snake.java

--- src/Snake.java
+++ src/Snake.java
@@ -1,30 +1,43 @@
 /**
- * Created by Tariq on 18.3.2014.
+ * Created by UK - Cheri, Kimberly and Ryan
  */
-public class Snake {
+public class Snake 
+{
 
     private Synchronizer synchronizer;
     private int exampleCounter;
 
-    public Snake(Synchronizer synchronizer) {
+    public Snake(Synchronizer synchronizer) 
+    {
         this.synchronizer = synchronizer;
     }
 
 
-    public void exampleUpdateFunction() {
-        if (exampleCounter > 5) {                                                                                   // if counter greater than five
-            System.out.println(" Snake.exampleUpdateFunction says: Yes! I'm finally going to kill that snake!");
-            synchronizer.setSnakeStillAlive(false);                                                                 // kill the little bugger
-        } else {
-            System.out.println(" Snake.exampleUpdateFunction says:  I'll let the snake live... for now.");
-        }
-
-        exampleCounter++;
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    public void exampleUpdateFunction() 
+    {
+      // if counter greater than five
+      if (exampleCounter > 5) 
+      {                          
+                System.out.println(" Snake.exampleUpdateFunction says: Yes! I'm finally going to kill that snake!");
+                synchronizer.setSnakeStillAlive(false);                                                                 
+            
+            else {
+                    System.out.println(" Snake.exampleUpdateFunction says:  I'll let the snake live... for now.");
+                 }
+    
+                exampleCounter++;
+    
+            try {
+                    Thread.sleep(1000);
+                }       
+                
+            catch (InterruptedException e) 
+            {
+                e.printStackTrace();
+            }
+      }
+   }
+   
+   
 }
+
