99a481de63ab3e43c3dc3f82be425310bb88dfce
erasmusuk <erasmus_uk@outlook.com>
2014-04-23 16:55:08 +0100

    Update Snake.java

--- src/Snake.java
+++ src/Snake.java
@@ -6,6 +6,17 @@ public class Snake
 
     private Synchronizer synchronizer;
     private int exampleCounter;
+    
+    //instance variable for placing snake
+    private char //note to self - finish this
+    
+    //positions the snake
+    private int x;
+    //positions the snake
+    private int y;
+    
+    
+    //
 
     public Snake(Synchronizer synchronizer) 
     {
@@ -37,6 +48,27 @@ public class Snake
             }
       }
    }
+   
+    public void placeSnake(){
+        //fix this tonight
+    	int X = (int)Math.random()*synchronizer.getGameAreaWidth();
+    	int Y = (int)Math.random()*synchronizer.getGameAreaHeight();
+    	boolean empty = false;
+
+    	while (empty == false) {
+    		if (synchronizer.getGameWorld()[X][Y]=='\0') {
+    			synchronizer.getGameWorld()[X][Y] = this.//insert array name from above later;
+    			empty = true;
+    		}
+    		else {
+    			X = (int)Math.random()*synchronizer.getGameAreaWidth();
+    			Y = (int)Math.random()*synchronizer.getGameAreaHeight();
+    		}
+    	}
+
+      this.x = X;
+      this.y = Y;
+    }
    
    
 }
