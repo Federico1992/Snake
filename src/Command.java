0ffe4ce4bc5c5b1fe01bbff0433bcd43e4f0d086
matteo1994 <matteo.marras1994@gmail.com>
2014-04-29 22:20:23 +0200

    Javadoc added

--- src/Command.java
+++ src/Command.java
@@ -1,4 +1,8 @@
-// Command
+/**
+ * Command class
+ * Istances of this class represent a kind of command insert by users.
+ * @author Italian People
+ */
 
 
 public abstract class Command{
@@ -9,10 +13,19 @@ public abstract class Command{
 		this.value = value;
 		}
 	
+	/** 
+	 * @return command's value
+	 */
 	public int getValue(){
 		return this.value;
 		}
 		
+	/**
+	 * Set value to this instance of Command.
+	 * @param value
+	 */
+	public void setValue(int value) {
+		this.value = value;
+		}
 
 	}
-
