f7a59d41cd218c584e5a064a95566f07915559c5
matteo1994 <matteo.marras1994@gmail.com>
2014-04-29 22:11:13 +0200

    Update Display.java

--- src/Display.java
+++ src/Display.java
@@ -1,11 +1,44 @@
-/**
- * Created by Tariq on 18.3.2014.
+/** 
+ * Display class.
+ * For score printing, we are waiting Synchronizer Class to implement getScore() method.
+ * @author Italian People
  */
 public class Display {
 
-    private Synchronizer synchronizer;
-
-    public Display(Synchronizer synchronizer) {
-        this.synchronizer = synchronizer;
-    }
+	private Synchronizer sync;
+	private int height, width;
+	
+	/**
+	 * Constructor with 1 parameter.
+	 * Inizialize parameter and set screen's size.
+	 * @param sync
+	 */
+	public Display (Synchronizer sync) {
+		this.sync=sync;
+		height = sync.getGameAreaHeight();
+		width = sync.getGameAreaWidth();
+	}
+	
+	/**
+	 * Refresh matrix and print game's screen.
+	 */
+	public void printGame () {
+		char[][] gameWorld = sync.getGameWorld();
+		//System.out.pritln("\nScore: " + sync.getScore() + "\n");
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < height; i++)
+				if (gameWorld[i][j] == '\0')
+					System.out.print(" ");
+				System.out.print(gameWorld[i][j]+" ");
+			System.out.println();
+		}
+	}
+	
+	private static void printLineOf (char c, int len){
+		for (int i=0; i<len; i++)
+			System.out.print(c);
+		System.out.println();
+	}
+		
+				
 }
